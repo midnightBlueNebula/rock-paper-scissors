f3ea43d9f9c49cd9c31e811ba467e3ebbb713d60
Onur Kaya <onurkaya8@outlook.com>
2019-07-15 23:42:17 +0300

    DOM manipulation added

--- game.js
+++ game.js
@@ -1,23 +1,34 @@
-var optionsArr = ['Rock','Paper','Scissors'];
-var index = Math.floor(Math.random()*3);
-var cpuSelected = optionsArr[index];
-var playerSelected = 'Rock'
-
-function computerPlay(playerSel,cpuSel){
+function computerPlay(event){
+    var optionsArr = ['Rock','Paper','Scissors'];
+    var index = Math.floor(Math.random()*3);
+    var cpuSel = optionsArr[index];
+    var playerSel = event.target.value;
     switch(playerSel.toLowerCase()){
         case 'rock':
-            if(cpuSel.toLowerCase()=='rock') return 'draw';
-            else if(cpuSel.toLowerCase()=='paper') return 'thou lost, paper wraps rock.';
-            else return 'You won! Rock beats scissors';break;
+            if(cpuSel.toLowerCase()=='rock') document.querySelector('#message').innerText = 'draw';
+            else if(cpuSel.toLowerCase()=='paper') document.querySelector('#message').innerText = 'thou lost, paper wraps rock.';
+            else document.querySelector('#message').innerText = 'You won! Rock beats scissors.';break;
         case 'paper':
-            if(cpuSel.toLowerCase()=='paper') return 'draw';
-            else if(cpuSel.toLowerCase()=='scissors') return 'thou lost, scissors clips paper.';
-            else return 'You won! Paper wraps rock.';break;
+            if(cpuSel.toLowerCase()=='paper') document.querySelector('#message').innerText = 'draw';
+            else if(cpuSel.toLowerCase()=='scissors') document.querySelector('#message').innerText = 'thou lost, scissors clips paper.';
+            else document.querySelector('#message').innerText = 'You won! Paper wraps rock.';break;
         case 'scissors':
-            if(cpuSel.toLowerCase()=='scissors') return 'draw';
-            else if(cpuSel.toLowerCase()=='rock') return 'thou lost, rock beats scissors.';
-            else return 'You won! Scissors clips paper.';break;         
+            if(cpuSel.toLowerCase()=='scissors') document.querySelector('#message').innerText = 'draw';
+            else if(cpuSel.toLowerCase()=='rock') document.querySelector('#message').innerText = 'thou lost, rock beats scissors.';
+            else document.querySelector('#message').innerText = 'You won! Scissors clips paper.';break;         
     }
+    var resultText = document.querySelector('#message').innerText;
+    if(resultText =='You won! Rock beats scissors.' || 
+    resultText =='thou lost, rock beats scissors.')
+        document.querySelector('#res-img').src='http://suptg.thisisnotatrueending.com/archive/18199837/images/1330873000683.jpg';
+    else if(resultText =='You won! Scissors clips paper.' || 
+    resultText =='thou lost, scissors clips paper.')
+        document.querySelector('#res-img').src='https://www.pinclipart.com/picdir/middle/0-3033_png-scissors-cutting-paper-scissors-clip-art-transparent.png';
+    else if(resultText == 'thou lost, paper wraps rock.' || 
+    resultText == 'You won! Paper wraps rock.')
+        document.querySelector('#res-img').src='https://3.bp.blogspot.com/-f3G8zOG39ZE/Wpty9YqqKkI/AAAAAAACg8I/8f813mebXQoCNKgedhU8sbxZLuoTykXiwCLcBGAs/s1600/e682b228b8ad1e1e8641de094f60e759.jpg';
+    else if(resultText=='')  document.querySelector('#res-img').src='';
+    else 
+        document.querySelector('#res-img').src='https://s3.amazonaws.com/tjn-blog-images/wp-content/uploads/2015/12/19235447/what-does-your-handshake-say-about-you-810x540.jpg';
 }
 
-console.log(computerPlay(playerSelected,cpuSelected));
